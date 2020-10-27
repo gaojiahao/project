460721a8dbfee7b56c5f403f034b70cb3ddbb5b7
gaojiahao <1026844853@qq.com>
2020-10-27 15:27:31 +0800

    feat（更改）

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
  * @Author: gaojiahao
  * @Date: 2020-10-19 15:27:12
  * @LastEditors: sueRimn
- * @LastEditTime: 2020-10-27 10:37:21
+ * @LastEditTime: 2020-10-27 15:27:10
 -->
 <template>
 <div class="layout">
