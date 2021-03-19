d5677a789bd81264c0d5de8a264898ed04f4852e
gaojiahao <1026844853@qq.com>
2021-03-19 17:53:39 +0800

    feat(产品派店修改)

--- src/views/sell/pieShowManager/addPieShow.vue
+++ src/views/sell/pieShowManager/addPieShow.vue
@@ -4,7 +4,7 @@
  * @Author: gaojiahao
  * @Date: 2020-11-11 09:56:05
  * @LastEditors: sueRimn
- * @LastEditTime: 2021-03-16 20:50:47
+ * @LastEditTime: 2021-03-19 14:49:23
 -->
 <template>
 <div>
@@ -22,8 +22,11 @@
                 <div class="ivu-form-item">
                     <label class="ivu-form-item-label" style="width: 120px;">选品人员:</label> 
                     <div class="ivu-form-item-content" style="margin-left: 120px;">
-                        <Table :columns="selectionPeopleColumns" :data="selectedGoods" height="300" width="820"></Table>
-                        <span style="margin-left: 10px;"></span>
+                        <Table :columns="selectionColumns" :data="selectedGoods" height="300" width="820"></Table>
+                        <span style="margin: 10px;">
+                            <Button size="small" @click="handleSelectAll(true)">全选</Button>
+                            <Button size="small" @click="handleSelectAll(false)">取消</Button>
+                        </span>
                     </div>
                 </div>
             </div>
@@ -31,21 +34,35 @@
                 <div class="ivu-form-item">
                     <label class="ivu-form-item-label" style="width: 120px;">未选人员:</label> 
                     <div class="ivu-form-item-content" style="margin-left: 120px;">
-                        <Table :columns="selectionPeopleColumns" :data="notSelectedGoods" height="300" width="820"></Table>
-                        <span style="margin-left: 10px;"></span>
+                        <Table :columns="notSelectedColumns" :data="notSelectedGoods" height="300" width="820"></Table>
+                        <span style="margin: 10px;">
+                            <Button size="small" @click="handleSelectAll2(true)">全选</Button>
+                            <Button size="small" @click="handleSelectAll2(false)">取消</Button>
+                        </span>
                     </div>
                 </div>
             </div>
             <div class="ivu-form ivu-form-label-right" style="    padding: 10px 10px 10px 10px;">
-            <div class="ivu-form-item">
-                <label class="ivu-form-item-label" style="width: 120px;">不选人员:</label> 
-                <div class="ivu-form-item-content" style="margin-left: 120px;">
-                    <Table :columns="selectionPeopleColumns" :data="dontSelectedGoods" height="300" width="820"></Table>
-                    <span style="margin-left: 10px;"></span>
+                <div class="ivu-form-item">
+                    <label class="ivu-form-item-label" style="width: 120px;">不选人员:</label> 
+                    <div class="ivu-form-item-content" style="margin-left: 120px;">
+                        <Table :columns="dontSelectedColumns" :data="dontSelectedGoods" height="300" width="820"></Table>
+                        <span style="margin: 10px;">
+                            <Button size="small" @click="handleSelectAll3(true)">全选</Button>
+                            <Button size="small" @click="handleSelectAll3(false)">取消</Button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="ivu-form ivu-form-label-right" style="    padding: 10px 10px 10px 10px;" v-if="dispatchedList.length">
+                <div class="ivu-form-item">
+                    <label class="ivu-form-item-label" style="width: 120px;">已派人员:</label> 
+                    <div class="ivu-form-item-content" style="margin-left: 120px;">
+                        <Table :columns="dispatchedColumns" :data="dispatchedList" height="300" width="820" ref="dontSelectedColumns"></Table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </div>
     <div class="top">
         <Divider orientation="left" size="small">审核信息</Divider>
@@ -78,7 +95,8 @@ import {
     CreateReviewAction
 } from "@service/tortExamineService";
 import {
-    GetPieShopById
+    GetPieShopById,
+    CreatePieShop
 } from "@service/sellService";
 
 import {
@@ -96,20 +114,19 @@ export default {
     mixins: [config,config2],
     data(){
         return{
-            selectionPeopleColumns: [
+            selectionColumns: [
                 {
-                    title: '派店',
-                    key: 'isCheck',
+                    title: '委派',
+                    key: 'status',
                     align: 'center',
                     render: (h, params) => {
                         return h('Checkbox', {
                             props: {
-                                single: false,
-                                value:  this.selectionPeopleData[params.index][params.column.key]
+                                value:  this.selectedGoods[params.index][params.column.key]
                             },
                             on: {
                                 'on-change': (event) => {
-                                    this.selectionPeopleData[params.index][params.column.key] = event;
+                                    this.selectedGoods[params.index][params.column.key] = event;
                                 }
                             }
                         });
@@ -127,16 +144,12 @@ export default {
                 },
                  {
                     title: '选品人',
-                    key: 'createName',
+                    key: 'selectName',
                     width:120
                 },
-                // {
-                //     title: '对应类目',
-                //     key: 'name'
-                // },
                 {
                     title: '选品时间',
-                    key: 'createOn',
+                    key: 'selectOn',
                     width:160
                 },
                 {
@@ -144,86 +157,19 @@ export default {
                     key: 'remark',
                 }
             ],
-            selectionPeopleData: [
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao",
-                    isCheck:true
-                },
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao",
-                    isCheck:true
-                },
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao",
-                    isCheck:true
-                },
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao",
-                    isCheck:true
-                },
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao",
-                    isCheck:true
-                },
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao",
-                    isCheck:true
-                },
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao",
-                    isCheck:true
-                },
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao",
-                    isCheck:true
-                },
-            ],
-            appointStoreColumns: [
+            notSelectedColumns: [
                  {
-                    title: '派店',
-                    key: 'isCheck',
+                    title: '委派',
+                    key: 'status',
                     align: 'center',
                     render: (h, params) => {
                         return h('Checkbox', {
                             props: {
-                                single: false,
-                                value:  this.appointStorePeopleData[params.index][params.column.key]
+                                value:  this.notSelectedGoods[params.index][params.column.key]
                             },
                             on: {
                                 'on-change': (event) => {
-                                    this.appointStorePeopleData[params.index][params.column.key] = event;
+                                    this.notSelectedGoods[params.index][params.column.key] = event;
                                 }
                             }
                         });
@@ -231,66 +177,102 @@ export default {
                 },
                 {
                     title: '平台',
-                    key: 'title',
+                    key: 'platformName',
                     width:120
                 },
                 {
                     title: '店铺',
-                    key: 'store',
+                    key: 'storeName',
                     width:120
                 },
                  {
                     title: '选品人',
-                    key: 'user',
+                    key: 'selectName',
                     width:120
                 },
-                // {
-                //     title: '对应类目',
-                //     key: 'name'
-                // },
                 {
                     title: '选品时间',
-                    key: 'date',
+                    key: 'selectOn',
                     width:160
                 },
                 {
                     title: '意见',
-                    key: 'address',
+                    key: 'remark',
                 }
             ],
-            appointStorePeopleData: [
-                {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao"
+            dontSelectedColumns: [
+                 {
+                    title: '委派',
+                    key: 'status',
+                    align: 'center',
+                    render: (h, params) => {
+                        return h('Checkbox', {
+                            props: {
+                                value:  this.dontSelectedGoods[params.index][params.column.key]
+                            },
+                            on: {
+                                'on-change': (event) => {
+                                    this.dontSelectedGoods[params.index][params.column.key] = event;
+                                }
+                            }
+                        });
+                    }
                 },
                 {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao"
+                    title: '平台',
+                    key: 'platformName',
+                    width:120
                 },
                 {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao"
+                    title: '店铺',
+                    key: 'storeName',
+                    width:120
+                },
+                 {
+                    title: '选品人',
+                    key: 'selectName',
+                    width:120
                 },
                 {
-                    title: "速卖通",
-                    address: '无',
-                    date: '2016-10-03',
-                    store:'亚马孙',
-                    user:"gaojiahao"
+                    title: '不选时间',
+                    key: 'selectOn',
+                    width:160
                 },
+                {
+                    title: '意见',
+                    key: 'remark',
+                }
+            ],
+            dispatchedColumns: [
+                {
+                    title: '平台',
+                    key: 'platformName',
+                    width:120
+                },
+                {
+                    title: '店铺',
+                    key: 'storeName',
+                    width:120
+                },
+                 {
+                    title: '选品人',
+                    key: 'sellerName',
+                    width:120
+                },
+                {
+                    title: '选品时间',
+                    key: 'createdOn',
+                    width:160
+                },
+                {
+                    title: '意见',
+                    key: 'remark',
+                }
             ],
             selectedGoods:[],
-            dontSelectedGoods:[],
-            notSelectedGoods:[]
+            dontSelectedGoods:[],  //不选人员
+            notSelectedGoods:[],    //未选
+            dispatchedList:[]  //已派
         }
     },
     methods: {
@@ -321,40 +303,53 @@ export default {
         goReturn(){
             this.$router.go(-1);
         },
-        save(status){
-            var pieShopId = this.$route.query.pieShopId;
-            var params = this.formValidate2;
-            params = {
-                ...params,
-                relatedId: pieShopId,
-                reviewResult:0,
-                reviewBefore: this.formValidate.status,
-                isPass: status
-            }
-            this.$refs['examine'].$refs['formValidate'].validate((valid) => {
-                if (valid) {
-                    if (params.relatedId) {
-                        return new Promise((resolve, reject) => {
-                            this.$FromLoading.show();
-                            CreateReviewAction(params).then(res => {
-                                if (res.result.code == 200) {
-                                    this.$FromLoading.hide();
-                                    this.$Message.info('温馨提示：审核成功！');
-                                    this.goReturn();
-                                } else if (res.result.code == 400) {
-                                    this.$Message.error({
-                                        background: true,
-                                        content: res.result.msg
-                                    });
-                                    this.$FromLoading.hide();
-                                }
+        save(){
+            var pieShops=[];
+            for(var i=0;i<this.selectedGoods.length;i++){
+                this.selectedGoods[i]['goodsId'] = this.formValidate['id'];
+                this.selectedGoods[i]['goodsName'] = this.formValidate['name'];
+                this.selectedGoods[i]['remark'] = this.formValidate2['reviewRemark'];
+                this.selectedGoods[i]['seller'] = this.selectedGoods[i]['selectBy'];
+                pieShops.push(this.selectedGoods[i]);
+            };
+            for(var i=0;i<this.dontSelectedGoods.length;i++){
+                this.dontSelectedGoods[i]['goodsId'] = this.formValidate['id'];
+                this.dontSelectedGoods[i]['goodsName'] = this.formValidate['name'];
+                this.dontSelectedGoods[i]['remark'] = this.formValidate2['reviewRemark'];
+                this.dontSelectedGoods[i]['seller'] = this.dontSelectedGoods[i]['selectBy'];
+                pieShops.push(this.dontSelectedGoods[i]);
+            };
+            for(var i=0;i<this.notSelectedGoods.length;i++){
+                this.notSelectedGoods[i]['goodsId'] = this.formValidate['id'];
+                this.notSelectedGoods[i]['goodsName'] = this.formValidate['name'];
+                this.notSelectedGoods[i]['remark'] = this.formValidate2['reviewRemark'];
+                this.notSelectedGoods[i]['seller'] = this.notSelectedGoods[i]['selectBy'];
+                pieShops.push(this.notSelectedGoods[i]);
+            };
+
+            if (pieShops.length) {
+                return new Promise((resolve, reject) => {
+                    this.$FromLoading.show();
+                    CreatePieShop({pieShops:pieShops}).then(res => {
+                        if (res.result.code == 200) {
+                            this.$FromLoading.hide();
+                            this.$Message.info('温馨提示：审核成功！');
+                            this.goReturn();
+                        } else  {
+                            this.$Message.error({
+                                background: true,
+                                content: res.result.msg
                             });
-                        });
-                    }
-                } else {
-                    this.$Message.error('保存失败');
-                }
-            })
+                            this.$FromLoading.hide();
+                        }
+                    });
+                });
+            } else {
+                this.$Message.error({
+                    background: true,
+                    content: '请选择派店人员'
+                });
+            }
         },
         GetPieShopById(){
             this.id = this.$route.query.id;
@@ -365,6 +360,16 @@ export default {
                             this.selectedGoods = res.result.item.selectedGoods;
                             this.dontSelectedGoods = res.result.item.dontSelectedGoods;
                             this.notSelectedGoods = res.result.item.notSelectedGoods;
+                            for(var i=0;i<this.selectedGoods.length;i++){
+                                this.$set(this.selectedGoods[i],'status',true);
+                            }
+                            for(var i=0;i<this.dontSelectedGoods.length;i++){
+                                this.$set(this.dontSelectedGoods[i],'status',false);
+                            }
+                            for(var i=0;i<this.notSelectedGoods.length;i++){
+                                this.$set(this.notSelectedGoods[i],'status',false);
+                            }
+                            this.dispatchedList = res.result.item.dispatchedList;
                         } else if (res.result.code == 400) {
                             this.$Message.error({
                                 background: true,
@@ -375,6 +380,21 @@ export default {
                 });    
             }
         },
+        handleSelectAll(status){
+            for(var i=0;i<this.selectedGoods.length;i++){
+                this.$set(this.selectedGoods[i],'status',status);
+            }
+        },
+        handleSelectAll2(status){
+            for(var i=0;i<this.notSelectedGoods.length;i++){
+                this.$set(this.notSelectedGoods[i],'status',status);
+            }    
+        },
+        handleSelectAll3(status){
+            for(var i=0;i<this.dontSelectedGoods.length;i++){
+                this.$set(this.dontSelectedGoods[i],'status',status);
+            }    
+        }
     },
     created() {
         this.getFormData();
